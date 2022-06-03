4c97a90b8bf59141d4a0026744b1ad7e50e25346
SG-Marco <rollingpoe@gmail.com>
2022-06-03 11:57:16 +0900

    feat - Make apps and models (users, wines, reviews, ratings)

---
+++ wine/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from user.models import UserModel
+from django.conf import settings
+
+
+# Create your models here.
+class WineModel(models.Model):
+    class Meta:
+        db_table = "wines"
+
+    name = models.CharField(max_length=256, default='')
+    producer = models.CharField(max_length=256, default='')
+    nation = models.CharField(max_length=256, default='')
+    local1 = models.CharField(max_length=256, default='')
+    local2 = models.CharField(max_length=256, default='')
+    local3 = models.CharField(max_length=256, default='')
+    local4 = models.CharField(max_length=256, default='')
+
+    varieties1 = models.CharField(max_length=256, default='')
+    varieties2 = models.CharField(max_length=256, default='')
+    varieties3 = models.CharField(max_length=256, default='')
+    varieties4 = models.CharField(max_length=256, default='')
+    varieties5 = models.CharField(max_length=256, default='')
+    varieties6 = models.CharField(max_length=256, default='')
+    varieties7 = models.CharField(max_length=256, default='')
+    varieties8 = models.CharField(max_length=256, default='')
+    varieties9 = models.CharField(max_length=256, default='')
+    varieties10 = models.CharField(max_length=256, default='')
+    varieties11 = models.CharField(max_length=256, default='')
+
+    year = models.IntegerField(default='')
+    type = models.CharField(max_length=256, default='')
+    degree = models.FloatField(max_length=256, default='')
+    sweet = models.IntegerField(default='')
+    acidity = models.IntegerField(default='')
+    body = models.IntegerField(default='')
+    tannin = models.IntegerField(default='')
+    price = models.IntegerField(default='')
+    av_rating = models.FloatField(max_length=256, default='')
+
+    wish = models.ManyToManyField(UserModel)
+
+
+
+class RatingModel(models.Model):
+    class Meta:
+        db_table = "ratings"
+
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    wine = models.ForeignKey(WineModel, on_delete=models.CASCADE)
+    rating = models.FloatField(max_length=256, default='')
+
+
+class ReviewModel(models.Model):
+    class Meta:
+        db_table = "reviews"
+
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    wine = models.ForeignKey(WineModel, on_delete=models.CASCADE)
+    rating = models.ForeignKey(RatingModel, on_delete=models.CASCADE)
+    content = models.TextField(max_length=2000, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
